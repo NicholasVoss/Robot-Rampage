5cfe56051cb5ce0ed677fc82ce6e5d0b953f44de
NicholasVoss <envy_456@yahoo.ca>
2020-11-03 15:55:18 -0500

    Chapter 10 - added enemies that shoot

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed = 30f;
+    public int damage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("deathTimer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the missile
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    IEnumerator deathTimer()
+    {
+        //destroy the missile if it does not hit anything after 10 seconds
+        yield return new WaitForSeconds(10);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+    [SerializeField]
+    GameObject missileprefab;
+
+    [SerializeField]
+    private string robotType;
+
+    public int health;
+
+    //vars for firing bullets
+    public int range;
+    public float fireRate;
+
+    //used for animations
+    public Animator robot;
+
+    public Transform missleFireSpot;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    private Transform player;
+    private float timeLastFired;
+
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+        //get components for navmesh agent and player
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dont continue if robot is dead
+        if(isDead)
+        {
+            return;
+        }
+
+        //robot looks towards player
+        transform.LookAt(player);
+
+        //use the navmesh to find a path to the player
+        agent.SetDestination(player.position);
+
+        //check if player is close enough to shoot and if enough time has passed since the last shot
+        //if true, fire
+        if(Vector3.Distance(transform.position, player.position) < range && Time.time - timeLastFired > fireRate)
+        {
+            timeLastFired = Time.time;
+            fire();
+        }
+    }
+
+    private void fire()
+    {
+        //creates and shoots a missile
+        GameObject missile = Instantiate(missileprefab);
+        missile.transform.position = missleFireSpot.transform.position;
+        missile.transform.rotation = missleFireSpot.transform.rotation;
+        robot.Play("Fire");
+    }
+}
