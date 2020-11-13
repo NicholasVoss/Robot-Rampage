5715639f4026ace8f92621b23b73913cda7177be
NicholasVoss <envy_456@yahoo.ca>
2020-11-13 14:39:09 -0500

    Chapter 11 - added background music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //prevent the object from being destroyed when switching scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
