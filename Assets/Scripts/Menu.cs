216bf2da5c8d45297637d3f2cccbcd92b5a12fdc
NicholasVoss <envy_456@yahoo.ca>
2020-11-13 14:31:10 -0500

    Chapter 11 - added start menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    //start game when player clicks start button
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+
+    //quits game when player clicks quit button
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
